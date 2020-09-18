8f14c431b70c3dbb7efb738c4d3877d7c5e963fc
Leah Scott-Zechlin <scottzec@gmail.com>
2020-09-18 17:25:29 +0200

    working code and test

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -2,6 +2,88 @@
 
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
+my_hand = VALID_CARDS.sample(rand(2..5))
+
+my_hand.each do |card|
+  if ! (VALID_CARDS.include?(card))
+    raise ArgumentError, "Invalid card"
+  end
+end
+
 def blackjack_score(hand)
+  score = 0
+
+  if hand.include?("Jack")
+    occur_jack = hand.count("Jack")
+    hand.delete("Jack")
+    hand << 10 * occur_jack
+  end
+
+  if hand.include?("Queen")
+    occur_queen = hand.count("Queen")
+    hand.delete("Queen")
+    hand << 10 * occur_queen
+  end
+
+  if hand.include?("King")
+    occur_king = hand.count("King")
+    hand.delete("King")
+    hand << 10 * occur_king
+  end
+
+  if hand.include?("Ace")
+    occur_ace = hand.count("Ace")
+    hand.delete("Ace")
+    temp_score = hand.reduce(0, :+)
+
+    if occur_ace == 1
+      if (0..10) === temp_score # === include?
+        hand << 11
+      elsif (11..20) === temp_score
+        hand << 1
+      else
+        raise ArgumentError, "Bust"
+      end
+    end
+
+    if occur_ace == 2
+      if (10..19) === temp_score
+        hand << 2 #(1 + 1)
+      elsif (0..9) === temp_score
+        hand << 12 #(11 + 1)
+      else
+        raise ArgumentError, "Bust"
+      end
+    end
+
+    if occur_ace == 3
+      if (9..18) === temp_score
+        hand << 3 # (1 + 1 + 1)
+      elsif (0..8) === temp_score
+        hand << 13 #(11 + 1 + 1)
+      else
+        raise ArgumentError, "Bust"
+      end
+    end
+
+    if occur_ace == 4
+      if (8..17) === temp_score
+        hand << 4 #(1 + 1 + 1 + 1)
+      elsif (0..7) === temp_score
+        hand << 14 #(11 + 1 + 1 + 1)
+      else
+        raise ArgumentError, "Bust"
+      end
+    end
+  end
+
+  score = hand.reduce(0, :+)
+
+  raise ArgumentError.new("Bust") if score > 21
+
+  return score
 
 end
+
+blackjack_score(my_hand)
+
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -8,6 +8,8 @@ require_relative '../lib/blackjack_score'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+
+
 describe 'Blackjack Score' do
   it 'can calculate the score for a pair of number cards' do
 
@@ -17,27 +19,83 @@ describe 'Blackjack Score' do
     # Act
     score = blackjack_score(hand)
 
-    # Assert <-  You do this part!
+    # Assert
+    expect(score).must_equal 7
 
   end
 
   it 'facecards have values calculated correctly' do
 
+    # Arrange
+    hand = ["King", "Queen"]
+
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 20
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
 
+    # Arrange
+    hand = ["Ace", 5, 3]
+
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 19
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
 
+    # Arrange
+    hand = ["Ace", 7, 5]
+
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 13
   end
 
   it 'raises an ArgumentError for invalid cards' do
 
+    # Arrange
+    hand = [13, 3, 5, "Queen"]
+
+    # Assert
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError, "Invalid Card"
   end
+  # The code isn't executing until the expect method wants it to. It's an argument for the expect method.
 
   it 'raises an ArgumentError for scores over 21' do
 
+    # Arrange
+    hand = [5, 3, 5, "Queen"]
+
+    # Act && Assert
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError, "Bust"
   end
+
 end
+
+
+# My sketch
+# describe "blackjack_score"
+# it "king and jack score" do
+#   # arrange
+#   hand = ["king", "jack"]
+#   # act
+#   score = count(hand)
+#   # assert
+#   expect(score).must_equal 20
+#   end
+#
+# # more tests in same format
+# end
